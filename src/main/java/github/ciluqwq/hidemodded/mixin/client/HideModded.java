61faee90ec0063b9eef3c082e534fe28018485e3
CiLuQwQ <out603484945@outlook.com>
2024-02-11 17:20:41 +0800

    FirstOld

--- src/main/java/github/ciluqwq/hidemodded/mixin/client/HideModded.java
+++ src/main/java/github/ciluqwq/hidemodded/mixin/client/HideModded.java
@@ -1,12 +1,10 @@
 package github.ciluqwq.hidemodded.mixin.client;
 
-import net.minecraft.util.ModStatus;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ModStatus.class)
+@Mixin(MinecraftClient.class)
 public class HideModded {
     /**
      * @author CiLuQwQ
